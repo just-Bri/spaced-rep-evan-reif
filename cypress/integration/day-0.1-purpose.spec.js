8d6ab34fbd00594f9a9dffa58355faef46c5c288
reif <bridgerrhammond@gmail.com>
2019-12-30 13:12:25 -0600

    sync after cyrpress

--- cypress/integration/day-0.1-purpose.spec.js
+++ cypress/integration/day-0.1-purpose.spec.js
@@ -10,12 +10,9 @@
 describe(`User story: Application name and purpose`, function() {
   it("has h1 with title", () => {
     cy.visit("/");
-    cy.get("header h1")
-      .should("contain", "Spaced repetition")
-      .get("p")
-      .should(
-        "have.text",
-        "Practice learning a language with the spaced reptition revision technique.",
-      );
+    cy.get("p").should(
+      "have.text",
+      "Practice learning a language with the spaced reptition revision technique.",
+    );
   });
 });
