deb1ba9fd2c0479c8d2d8b72507aac2bdd69bb3a
Evan Vogts <evanjvogts@gmail.com>
2019-12-30 11:21:34 -0600

    added form stylings

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
+import './RegistrationRoute.css'
 
 class RegistrationRoute extends Component {
   static defaultProps = {
@@ -16,10 +17,13 @@ class RegistrationRoute extends Component {
   render() {
     return (
       <section>
-        <p>
+        <p className='signup-subtitle'>
           Practice learning a language with the spaced reptition revision technique.
         </p>
-        <h2>Sign up</h2>
+        <h2
+          className='signup-form-title'>
+            Sign up
+        </h2>
         <RegistrationForm
           onRegistrationSuccess={this.handleRegistrationSuccess}
         />
