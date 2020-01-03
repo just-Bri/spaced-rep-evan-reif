c305f46d9c361533f0d6694e022af9e2c6097493
Evan Vogts <evanjvogts@gmail.com>
2020-01-03 15:10:23 -0600

    merge

--- src/config.js
+++ src/config.js
@@ -1,3 +1,8 @@
 export default {
+<<<<<<< HEAD
   API_ENDPOINT: "https://latin-translator-api-evan-reif.herokuapp.com/api",
+=======
+  API_ENDPOINT: "https://latin-evan-reif.herokuapp.com/api",
+  API_KEY: "dummy-token",
+>>>>>>> 1074da7b275173cc4a5458a56fd1160e4e8729c6
 };
